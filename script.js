a38fbb70d2d9c5ae57124144edf6c54e7ff14577
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2026-06-10 15:49:56 -0400

    added a dark mode button

--- script.js
+++ script.js
@@ -81,3 +81,28 @@ welcomeClose.addEventListener("click", () => {
 welcomeOpen.addEventListener("click", () => {
     welcomeScreen.style.display = "block";
 });
+
+// dark mode toggle
+
+// selectors
+const themeToggleBtn = document.querySelector('.theme-toggle');
+
+// state
+const theme = localStorage.getItem('theme');
+
+// on mount
+theme && document.body.classList.add(theme);
+
+
+// handlers
+handleThemeToggle = () => {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark-mode');
+    } else {
+        localStorage.removeItem('theme');
+    }
+};
+
+// events
+themeToggleBtn.addEventListener('click', handleThemeToggle);
